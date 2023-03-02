373ca476aa3f127607d7eed8160ed1e490d0723e
Gustavo_H <gustavo.hilario@automattic.com>
2023-03-02 16:48:06 -0500

    Change read More

--- plugins/test.php
+++ plugins/test.php
@@ -10,5 +10,8 @@
  **/
 
 
-/* Disable WordPress Admin Bar for all users */
-add_filter('show_admin_bar', '__return_false');
+function modify_read_more_link()
+{
+    return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+}
+add_filter('the_content_more_link', 'modify_read_more_link');
