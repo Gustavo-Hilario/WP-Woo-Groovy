4fed698c7387ee4c46ed1f75c0e852d12e250fb0
Gustavo_H <gustavo.hilario@automattic.com>
2023-03-02 16:57:22 -0500

    Adding plugin file to folder

---
+++ plugins/my-test-plugin/test.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: test-gustavo
+ * Plugin URI: https://woo.groovytestsite.blog/
+ * Description: Test.
+ * Version: 0.1
+ * Author: Gustavo-Hilario
+ * Author URI: https://woo.groovytestsite.blog/
+ **/
+
+function disable_wpcomtoolbar($modules)
+{
+   if (isset($modules['masterbar'])) {
+      unset($modules['masterbar']);
+   }
+   return $modules;
+}
+add_filter('jetpack_get_available_modules', 'disable_wpcomtoolbar');
