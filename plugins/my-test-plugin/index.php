c19096f89f0b85926d27fa9ae3e3ab89ca7e3286
Gustavo_H <gustavo.hilario@automattic.com>
2023-03-02 17:00:34 -0500

    Adding plugin file to folder

--- plugins/my-test-plugin/index.php
+++ plugins/my-test-plugin/index.php
@@ -9,11 +9,19 @@
  * Author URI: https://woo.groovytestsite.blog/
  **/
 
-function disable_wpcomtoolbar($modules)
+function wpb_follow_us($content)
 {
-   if (isset($modules['masterbar'])) {
-      unset($modules['masterbar']);
+
+   // Only do this when a single post is displayed
+   if (is_single()) {
+
+      // Message you want to display after the post
+      // Add URLs to your own Twitter and Facebook profiles
+
+      $content .= '<p class="follow-us">If you liked this article, then please follow us on <a href="http://twitter.com/wpbeginner" title="WPBeginner on Twitter" target="_blank" rel="nofollow">Twitter</a> and <a href="https://www.facebook.com/wpbeginner" title="WPBeginner on Facebook" target="_blank" rel="nofollow">Facebook</a>.</p>';
    }
-   return $modules;
+   // Return the content
+   return $content;
 }
-add_filter('jetpack_get_available_modules', 'disable_wpcomtoolbar');
+// Hook our function to WordPress the_content filter
+add_filter('the_content', 'wpb_follow_us');
