4eb1d3489f654cf6f7894f833598777069bb0f0c
Gustavo_H <gustavo.hilario@automattic.com>
2023-03-02 16:45:40 -0500

    Hide Admin Bar

--- plugins/test.php
+++ plugins/test.php
@@ -1,1 +1,14 @@
-printf("Test");
+<?php
+
+/**
+ * Plugin Name: test-gustavo
+ * Plugin URI: https://woo.groovytestsite.blog/
+ * Description: Test.
+ * Version: 0.1
+ * Author: Gustavo-Hilario
+ * Author URI: https://woo.groovytestsite.blog/
+ **/
+
+
+/* Disable WordPress Admin Bar for all users */
+add_filter('show_admin_bar', '__return_false');
