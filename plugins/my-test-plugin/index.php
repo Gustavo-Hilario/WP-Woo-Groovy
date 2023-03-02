38ad95a01e3a04b578e5cb5d629d9b61e800f2e9
Gustavo_H <gustavo.hilario@automattic.com>
2023-03-02 17:15:41 -0500

    Testing

--- plugins/my-test-plugin/index.php
+++ plugins/my-test-plugin/index.php
@@ -9,8 +9,19 @@
  * Author URI: http://gustavohilario.com/
  **/
 
-function modify_read_more_link()
+function wpb_follow_us($content)
 {
-   return '<a class="more-link" href="' . get_permalink() . '">Click to Read!</a>';
+
+   // Only do this when a single post is displayed
+   if (is_single()) {
+
+      // Message you want to display after the post
+      // Add URLs to your own Twitter and Facebook profiles
+
+      $content .= '<p class="follow-us">If you liked this article, then please follow us on <a href="http://twitter.com/wpbeginner" title="WPBeginner on Twitter" target="_blank" rel="nofollow">Twitter</a> and <a href="https://www.facebook.com/wpbeginner" title="WPBeginner on Facebook" target="_blank" rel="nofollow">Facebook</a>.</p>';
+   }
+   // Return the content
+   return $content;
 }
-add_filter('the_content_more_link', 'modify_read_more_link');
+// Hook our function to WordPress the_content filter
+add_filter('the_content', 'wpb_follow_us');
